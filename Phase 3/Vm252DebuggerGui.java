a5c0baa3aa2755c3ce25b9b48b876f10f2914aeb
Phat Tu <77705393+sturekev@users.noreply.github.com>
2022-11-01 09:30:18 -0500

    add java and opninion folder for discusion

---
+++ Phase 3/Vm252DebuggerGui.java
@@ -0,0 +1,38 @@
+//
+//
+//
+//
+// Project phase 2 
+// team member: Anirudh Chauhan, Owen Gruenwald, Phat Tu
+//
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.util.*;
+
+import javax.lang.model.util.ElementScanner14;
+import javax.swing.*;
+
+public class Vm252DebuggerGui{
+    public static main(String [] commandLineArguments){
+        EventQueue.invokeLater(
+            () ->{
+                ProgramFrame frame = new ProgramFrame();
+
+                frame.setTitle("Better Mouse Test");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        );
+    }
+}
+
+/**
+ * InnerVm252DebuggerGui
+ * working for create button and processings
+ */
+public class InnerVm252DebuggerGui {
+
+    
+}
